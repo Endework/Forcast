54416109ade076795c13fd24d8628883cac9912e
Endework <endeworkabera@gmail.com>
2022-05-27 01:59:00 -0700

    new update

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -11,11 +11,11 @@ X, y = make_classification(n_samples = 1000, random_state=seed)
 X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=seed)
 
 # Save it
-if not os.path.isdir("data"):
-    os.mkdir("data")
-np.savetxt("data/AdSmartABdata.csv",X_train)
-np.savetxt("data/AdSmartABdata.csv",X_test)
-np.savetxt("data/AdSmartABdata.csv",y_train)
-np.savetxt("data/AdSmartABdata.csv",y_test)
+if not os.path.isdir("Data"):
+    os.mkdir("defaultdata")
+np.savetxt("defaultdata/train.csv",X_train)
+np.savetxt("defaultdata/test.csv",X_test)
+np.savetxt("defaultdata/train.csv",y_train)
+np.savetxt("defaultdata/test.csv",y_test)
 
 
